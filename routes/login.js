025821bda76654dde8fba592c9e4abb7bc29b3cf
adyapathak26 <adyapathak026@gmail.com>
2023-08-04 18:32:26 +0530

    error in login file

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,13 @@
-const express =require("express")
+const express = require("express")
 const router = express.Router()
+const fs= require("fs")
 router.use(express.json())
 router.use(express.urlencoded({ extended: true }));
 router.post("/",(req,res)=>{
-    const data = req.body
-    res.send(data)
+    console.log("login")
+
+    
+    
 
     
 })
