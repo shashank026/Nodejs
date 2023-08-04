b6539a8ed4730dd1c3fce9386ba02dc2f4df3b45
Shashank <72060531+shashank026@users.noreply.github.com>
2023-08-04 19:11:27 +0530

    fix the issue

--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,27 @@
 const express = require("express")
 const router = express.Router()
-const fs= require("fs")
+const userDetails = require('../user_details.json');
 router.use(express.json())
 router.use(express.urlencoded({ extended: true }));
-router.post("/",(req,res)=>{
-    console.log("login")
+router.post("/", (req, res) => {
+    try {
+        const { username, password } = req.body;
+        if (username && password) {
+            for (let i = 0; i < userDetails.length; i++) {
+                if (userDetails[i].username === username && userDetails[i].password === password) {
 
-    
-    
+                    res.send("User verified");
 
-    
-})
+                } else if (userDetails[i].username === username && userDetails[i].password !== password) {
+                    res.send("Invalid credentials");
+                }
+            }
+            res.send("User not found");
+        }
+        res.send("Username and Password is required");
+    } catch (error) {
+        res.status(500).send("Internal Server Error");
+    }
+});
 
-module.exports=router
+module.exports = router
