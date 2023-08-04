80a1a5b0a96066595427262de20272a38abd8240
adyapathak26 <adyapathak026@gmail.com>
2023-08-04 23:17:42 +0530

    signup router fix

--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,36 @@
 const express = require("express")
 const router = express.Router()
-const fs= require("fs")
+const userDetails=require('../user_details.json');
 router.use(express.json())
 router.use(express.urlencoded({ extended: true }));
 router.post("/",(req,res)=>{
-    console.log("login")
-
+    try {
+     const{username,password}=req.body;
+     if(username && password)
+     {
+          for(let i=0;i<userDetails.length;i++)
+          {
+               if(userDetails[i].username==username && userDetails[i].password===password)
+               {
+                    res.send("User verified");
+               }
+               else if(userDetails[i].username===username && userDetails[i].password!==password)
+               {
+                    res.send("invalid credentials");
+               }
+          }
+          res.send("user not found");
+          }
+          res.send("username and password is required");
+     }
+     catch(error)
+     {
+          res.status(500).send("Internal server error");
+     }
+    });
     
     
 
     
-})
 
 module.exports=router
